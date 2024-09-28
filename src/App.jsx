2c1b9ed6923ad8bb5e90dea882ee1495302a1ea0
Nirmal lowanshi <nirmallowanshi50@gmail.com>
2024-09-28 17:38:29 +0530

    Show signup form

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import LoginForm from './Component/LoginForm' 
+import SignupForm from './Component/SignupForm'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <LoginForm />
+    <SignupForm />
 
     </>
   )
